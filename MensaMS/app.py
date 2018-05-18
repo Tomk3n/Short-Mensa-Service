3eb734c1a730632b372769d2df4e11275cc8782e
betaros <betaros@gmx.de>
2018-05-18 17:16:48 +0200

    Mensa Parser erweitert

--- MensaMS/app.py
+++ MensaMS/app.py
@@ -1,20 +1,30 @@
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields, marshal
 from bs4 import BeautifulSoup
 import urllib.request
 import json
 
-from pip.utils import encoding
-
 app = Flask(__name__)                  #  Create a Flask WSGI application
 api = Api(app)                         #  Create a Flask-RESTPlus API
 
-ns = api.namespace('mensa', description='Mensa Speiseplan Microservice')
+ns = api.namespace('mensa', title='Mensa Microservice', description='This microservice returns the current menu')
 
-@ns.route('/data')
+@ns.route('/getter')
 class Data(Resource):
     def get(self):
-        url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/'
+        # debug sites
+        # ---------------------------
+        # added notice
+        # url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/?datum=2018-05-12'
+
+        # normal day
+        #url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/?datum=2018-05-18'
+
+        # no data
+        url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/?datum=2018-05-21'
+        # ---------------------------
+
+        #url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/'
         text = urllib.request.urlopen(url).read().decode('utf-8')
 
         parsed_html = BeautifulSoup(text, "html.parser")
@@ -23,19 +33,35 @@ class Data(Resource):
         string = []
         for i in result:
             element_result = []
-            menu = i.find('td', attrs={'style':'width:70%'}).text
 
-            price = i.find_all('td', attrs={'class':'hidden-xs'})
-            pricelist = []
-            for j in price:
-                pricelist.append(j.text.replace("\xa0€", ""))
-                element_result.append(menu.replace("\r\n  ", "").replace("\xa0€", ""))
+            rows = i.find_all('tr')
+            for row in rows:
+                menu = row.find('td', attrs={'style':'width:70%'})
+
+                if not menu:
+                    continue
+
+                menu_name = menu.text.split('(', 1)[0]
+                print(menu_name)
+
+                element_result.append(menu_name.replace("\r\n  ", "").replace("\xa0€", ""))
+
+                price = row.find_all('td', attrs={'class':'hidden-xs', 'style':'text-align:center'})
+                pricelist = []
+                for j in price:
+                    pricelist.append(j.text.replace("\xa0€", ""))
+
                 element_result.append(pricelist)
             string.append(element_result)
 
         for i in string:
             print(i)
-        return json.dumps(string, sort_keys=True, indent=4, ensure_ascii=False)
+
+        if not string:
+            return None, 204
+        else:
+            return string
+            #return json.dumps(marshal(string), indent=4)
 
 
 if __name__ == '__main__':
