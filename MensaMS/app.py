bc49d6db52c7ac0c34a636448c249e00faec6b1d
betaros <betaros@gmx.de>
2018-05-20 15:58:20 +0200

    Bugfix MensaMS

--- MensaMS/app.py
+++ MensaMS/app.py
@@ -19,13 +19,13 @@ class Data(Resource):
         # url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/?datum=2018-05-12'
 
         # normal day
-        # url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/?datum=2018-05-18'
+        url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/?datum=2018-05-18'
 
         # no data
         # url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/?datum=2018-05-21'
         # ---------------------------
 
-        url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/'
+        # url = 'http://studwerk.fh-stralsund.de/essen/speiseplaene/mensa-stralsund/'
         text = urllib.request.urlopen(url).read().decode('utf-8')
 
         parsed_html = BeautifulSoup(text, "html.parser")
@@ -36,6 +36,8 @@ class Data(Resource):
         for i in result:
             component = i.find_all('th')
 
+            mensa_obj[component[0].text.rstrip()] = []
+
             rows = i.find_all('tr')
             for row in rows:
                 menu = row.find('td', attrs={'style': 'width:70%'})
@@ -44,8 +46,8 @@ class Data(Resource):
                     continue
 
                 menu_obj = {}
-                menu_name = menu.text.split('(', 1)[0]
-                menu_obj["Gericht"] = menu_name
+                menu_name = menu.text.split('(', 1)[0].split(' \n', 1)[0]
+                menu_obj["Gericht"] = menu_name.replace("\r\n  ", "").rstrip()
 
                 price = row.find_all('td', attrs={'class': 'hidden-xs', 'style': 'text-align:center'})
                 pricelist = []
@@ -56,7 +58,7 @@ class Data(Resource):
                 menu_obj["Mitarbeiter"] = pricelist[1]
                 menu_obj["Gast"] = pricelist[2]
 
-                mensa_obj[component[0].text] = menu_obj
+                mensa_obj[component[0].text.rstrip()].append(menu_obj)
 
         if not mensa_obj:
             return None, 204
